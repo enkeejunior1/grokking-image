4e813dd683a19ce5575fcbee3536a1f7ccc397a0
Joon Hyeok Kim <hozy@grasp-login1.grasp.maas>
2025-11-27 13:06:15 -0500

    run log and bash script added

---
+++ mnist/sbatch_scripts/run_zero_out.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Slurm setup
+#SBATCH -p gu-compute
+#SBATCH -A gu-account
+#SBATCH --qos=gu-med
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH --time=4:00:00
+#SBATCH --output=/dev/null
+#SBATCH --error=/dev/null
+
+mkdir -p "./logs"
+
+DATE_WITH_TIME=$(date +%Y%m%d_%H%M%S)
+OUTPUT_FILE="./logs/${DATE_WITH_TIME}_run_${SLURM_JOB_ID}.log"
+
+exec > "$OUTPUT_FILE" 2>&1
+
+echo "==================================="
+echo "min_mdlm run"
+date
+echo "Job running on node: $(hostname)"
+echo "==================================="
+
+echo "Attempting to activate venv"
+source ../venv/bin/activate
+
+echo "[DEBUG] Python check:"
+python --version
+echo "==================================="
+echo "Running attention_head_test.py starts"
+date
+
+python -m attention_head_test --zero-out >> "$OUTPUT_FILE" 2>&1
+
+echo "==================================="
+echo "Fin."
+date
